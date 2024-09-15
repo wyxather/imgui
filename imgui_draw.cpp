4cb3bfc5eff21c5e47cf939ca0ccee663c48b10b
thedmd <michcic@gmail.com>
2024-09-16 00:34:03 +0200

    ImDrawList: DIrect implementation of AddRect() (WIP) (fixup)

--- imgui_draw.cpp
+++ imgui_draw.cpp
@@ -3441,6 +3441,8 @@ if (GImGui->IO.KeyCtrl || (flags & 0x80000000u))
     {
         inner_color = col;
         outer_color = col & ~IM_COL32_A_MASK;
+        outer_thickness = thickness * 2.0f;
+        strip_count = 2;
 
         if (thickness > this->_FringeScale) IM_LIKELY
         {
@@ -3455,13 +3457,7 @@ if (GImGui->IO.KeyCtrl || (flags & 0x80000000u))
             if (alpha == 0)
                 return;
 
-            strip_count     = 2;
-            inner_color     = outer_color | (alpha << IM_COL32_A_SHIFT);
-            outer_thickness = this->_FringeScale;
-        }
-        else IM_LIKELY
-        {
-            strip_count = 2;
+            inner_color = outer_color | (alpha << IM_COL32_A_SHIFT);
         }
     }
 
@@ -3484,6 +3480,8 @@ if (GImGui->IO.KeyCtrl || (flags & 0x80000000u))
         strip_size = 4;
 
         IM_POLYLINE_PRIM_RESERVE(24 * strip_count, 4 * (strip_count + 1));
+        IM_UNUSED(IM_POLYLINE_PRIM_IDX_BASE);
+        IM_UNUSED(IM_POLYLINE_PRIM_IDX_WRITE);
 
 #define IM_ADDRECT_EMIT_STRIP_VERTICES(AX, AY, BX, BY, C) { IM_POLYLINE_VERTEX(0, AX, AY, uv, C); IM_POLYLINE_VERTEX(1, BX, AY, uv, C); IM_POLYLINE_VERTEX(2, BX, BY, uv, C); IM_POLYLINE_VERTEX(3, AX, BY, uv, C); IM_POLYLINE_PRIM_VTX_WRITE += 4; }
 
@@ -3497,15 +3495,16 @@ if (GImGui->IO.KeyCtrl || (flags & 0x80000000u))
     else
     {
         const int arc_step = ImMax(1, IM_DRAWLIST_ARCFAST_SAMPLE_MAX / _CalcCircleAutoSegmentCount(rounding));
+        const float arc_offset_correction = 0.207107f * _FringeScale; // sqrt(2)/4
 
 #define IM_ADDRECT_EMIT_ARC(P, R, A_MIN, A_MAX) _PathArcToFastEx(P, R, (A_MIN) * IM_DRAWLIST_ARCFAST_SAMPLE_MAX / 12, (A_MAX) * IM_DRAWLIST_ARCFAST_SAMPLE_MAX / 12, arc_step)
-#define IM_ADDRECT_EMIT_ROUNDED_PATH(AX, AY, BX, BY, ROUNDING)                                                                                             \
-        {                                                                                                                                                  \
-            const float r = ImMax(ROUNDING, 0.5f);                                                                                                         \
-            if ((flags & ImDrawFlags_RoundCornersTopLeft))     IM_ADDRECT_EMIT_ARC(ImVec2((AX) + r, (AY) + r), r, 6,  9); else PathLineTo(ImVec2(AX, AY)); \
-            if ((flags & ImDrawFlags_RoundCornersTopRight))    IM_ADDRECT_EMIT_ARC(ImVec2((BX) - r, (AY) + r), r, 9, 12); else PathLineTo(ImVec2(BX, AY)); \
-            if ((flags & ImDrawFlags_RoundCornersBottomRight)) IM_ADDRECT_EMIT_ARC(ImVec2((BX) - r, (BY) - r), r, 0,  3); else PathLineTo(ImVec2(BX, BY)); \
-            if ((flags & ImDrawFlags_RoundCornersBottomLeft))  IM_ADDRECT_EMIT_ARC(ImVec2((AX) + r, (BY) - r), r, 3,  6); else PathLineTo(ImVec2(AX, BY)); \
+#define IM_ADDRECT_EMIT_ROUNDED_PATH(AX, AY, BX, BY, ROUNDING)                                                                                                                                             \
+        {                                                                                                                                                                                                  \
+            const float r = ImMax(ROUNDING, 0.5f);                                                                                                                                                         \
+            if ((flags & ImDrawFlags_RoundCornersTopLeft))     IM_ADDRECT_EMIT_ARC(ImVec2((AX) + r + arc_offset_correction, (AY) + r + arc_offset_correction), r, 6,  9); else PathLineTo(ImVec2(AX, AY)); \
+            if ((flags & ImDrawFlags_RoundCornersTopRight))    IM_ADDRECT_EMIT_ARC(ImVec2((BX) - r - arc_offset_correction, (AY) + r + arc_offset_correction), r, 9, 12); else PathLineTo(ImVec2(BX, AY)); \
+            if ((flags & ImDrawFlags_RoundCornersBottomRight)) IM_ADDRECT_EMIT_ARC(ImVec2((BX) - r - arc_offset_correction, (BY) - r - arc_offset_correction), r, 0,  3); else PathLineTo(ImVec2(BX, BY)); \
+            if ((flags & ImDrawFlags_RoundCornersBottomLeft))  IM_ADDRECT_EMIT_ARC(ImVec2((AX) + r + arc_offset_correction, (BY) - r - arc_offset_correction), r, 3,  6); else PathLineTo(ImVec2(AX, BY)); \
         }
 #define IM_ADDRECT_PATH_TO_VERTICES(C)                                        \
         for (int i = 0; i < this->_Path.Size; i++)                            \
@@ -3517,6 +3516,8 @@ if (GImGui->IO.KeyCtrl || (flags & 0x80000000u))
         IM_ADDRECT_EMIT_ROUNDED_PATH(a.x - half_outer_thickness, a.y - half_outer_thickness, b.x + half_outer_thickness, b.y + half_outer_thickness, rounding + half_outer_thickness);
         strip_size = this->_Path.Size;
         IM_POLYLINE_PRIM_RESERVE(strip_size * strip_count * 6, strip_size * (strip_count + 1));
+        IM_UNUSED(IM_POLYLINE_PRIM_IDX_BASE);
+        IM_UNUSED(IM_POLYLINE_PRIM_IDX_WRITE);
         IM_ADDRECT_PATH_TO_VERTICES(outer_color);
 
         if (strip_count >  1) IM_LIKELY   IM_ADDRECT_EMIT_ROUNDED_VERTICES(a.x - half_inner_thickness, a.y - half_inner_thickness, b.x + half_inner_thickness, b.y + half_inner_thickness, rounding + half_inner_thickness, inner_color);
