13576f20c42277c878d0549b95242cb2096fac73
wyxather <79754485+wyxather@users.noreply.github.com>
2024-09-28 13:19:46 +0700

    Stack Layout implementation v2.1 (Dynamic States)

--- imgui_stacklayout.h
+++ imgui_stacklayout.h
@@ -20,6 +20,8 @@ Index of this file:
 
 namespace ImGui
 {
+    IMGUI_API void CreateStackLayoutStates();
+    IMGUI_API void DestroyStackLayoutStates();
     IMGUI_API void BeginHorizontal(const char* str_id, const ImVec2& size = ImVec2(0, 0), float align = -1.0f);
     IMGUI_API void BeginHorizontal(const void* ptr_id, const ImVec2& size = ImVec2(0, 0), float align = -1.0f);
     IMGUI_API void BeginHorizontal(int id, const ImVec2& size = ImVec2(0, 0), float align = -1);
